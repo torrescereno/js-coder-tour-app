d27512f13cef41509f342604d3633b9de7d70cdf
Luis <torrescereno@gmail.com>
2020-12-10 23:01:58 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function msjAlert(){
+
+    let email = prompt('Ingresa tu correo si quieres recibir información de promociones');
+
+    const emailRegex = new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")
+
+    while(true){
+
+        if (email !== '') {
+            if (emailRegex.exec(email)) {
+                alert('Gracias!!');
+                break;
+            }else{
+                email = prompt('El email ingresado no es valido, por favor ingreselo nuevamenteasd');
+            }
+        }else{
+            break;
+        }
+    }
+};
+
+//msjAlert();
